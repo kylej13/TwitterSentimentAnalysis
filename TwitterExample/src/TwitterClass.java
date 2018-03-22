0d27d48c71a82d46fb0c2f6fa6119f0e39e5d128
Kyle Johnson <kyle.johnson@hcl.com>
2018-03-22 14:48:53 -0400

    Outputting overall document scores

--- TwitterExample/src/TwitterClass.java
+++ TwitterExample/src/TwitterClass.java
@@ -16,6 +16,7 @@ import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.DocumentAnalysis;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.SentenceAnalysis;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneCategory;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneOptions;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneScore;
 
@@ -33,8 +34,21 @@ public class TwitterClass {
 	private static String ConsumerSecret = "TI10f56Awm7r8NVkSAdBHMoIWQFRJbXzQKQun6nYMLZkk2QWow";
 
 	public static void main(String[] args) {
-		String url = createRequestURLForUser("BarackObama");
 
+		String allTweetsText = retrieveTweetsFromUsername("BarackObama");
+		ToneAnalysis result = analyzeText(allTweetsText);
+
+		DocumentAnalysis da = result.getDocumentTone();
+		System.out.println("\n\n Document score");
+		List<ToneScore> toneScores = da.getTones();
+
+		for (ToneScore score : toneScores) {
+			System.out.println(score.getToneName() + ": " + score.getScore());
+		}
+	}
+
+	private static String retrieveTweetsFromUsername(String username) {
+		String url = createRequestURLForUsername(username);
 		OAuthConsumer consumer = new CommonsHttpOAuthConsumer(ConsumerKey, ConsumerSecret);
 		consumer.setTokenWithSecret(AccessToken, AccessSecret);
 
@@ -59,6 +73,8 @@ public class TwitterClass {
 		}
 		String response = sb.toString();
 
+		StringBuilder allTweets = new StringBuilder();
+
 		try {
 			JSONObject obj = new JSONObject(response);
 			JSONArray statuses = obj.getJSONArray("statuses");
@@ -66,76 +82,32 @@ public class TwitterClass {
 			for (int x = 0; x < statuses.length(); x++) {
 				JSONObject tweet = (JSONObject) statuses.get(x);
 				String tweetText = tweet.getString("full_text");
-				sb.append(tweetText + " ");
+				allTweets.append(tweetText + " ");
 			}
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 
-		String allTweetsText = sb.toString();
-		// JSONObject watsonRequest = createWatsonJSON(allTweetsText);
+		return allTweets.toString();
+	}
 
+	private static ToneAnalysis analyzeText(String text) {
 		ToneAnalyzer service = new ToneAnalyzer("2017-09-21", "d74d1640-78d7-4c0b-ba88-ed16e9935e2d", "05icUbUbAXrJ");
-		ToneOptions toneOptions = new ToneOptions.Builder().html(allTweetsText).addTone(ToneOptions.Tone.EMOTION)
-				.addTone(ToneOptions.Tone.LANGUAGE).addTone(ToneOptions.Tone.SOCIAL).build();
+		ToneOptions toneOptions = new ToneOptions.Builder().html(text).addTone(ToneOptions.Tone.EMOTION).build();
 		System.out.println("Tones:");
 		for (String tone : toneOptions.tones()) {
 			System.out.println(tone);
 		}
 		ToneAnalysis result = service.tone(toneOptions).execute();
 		System.out.println("\n\n Sentences score");
-		for (SentenceAnalysis sa : result.getSentencesTone()) {
-			for (ToneScore ts : sa.getTones()) {
-				System.out.println(ts.getToneName() + ": " + ts.getScore());
-			}
-		}
 
-		// System.out.println(result);
-
+		return result;
 	}
 
-	/**
-	 * public static void main(String[] args) { ConfigurationBuilder cb = new
-	 * ConfigurationBuilder();
-	 * cb.setDebugEnabled(true).setOAuthConsumerKey("gMQMgbfwB7tIQ4ywoEDyp7CR9")
-	 * .setOAuthConsumerSecret("TI10f56Awm7r8NVkSAdBHMoIWQFRJbXzQKQun6nYMLZkk2QWow")
-	 * .setOAuthAccessToken("926144203071115265-ZBiOBu4tRo617dGu2kJuOTfuBMoApEg")
-	 * .setOAuthAccessTokenSecret("Y42ysR3XXd8k5YAwnUcdQAXwQLYR2FSqrvfZ14t2s7IXn");
-	 * TwitterFactory tf = new TwitterFactory(cb.build()); Twitter twitter =
-	 * tf.getInstance();
-	 * 
-	 * Query query = new Query("source:twitter4j baeldung"); QueryResult result; try
-	 * { //Paging paging = new Paging(1, 1); List<Status> statuses =
-	 * twitter.getUserTimeline("realDonaldTrump"); //twitter.getUserT
-	 * 
-	 * // result = twitter.search(query); System.out.println("Search results: " +
-	 * statuses.size()); for (Status status : statuses) { status. //status.get
-	 * System.out.println(status.getText()); JSONObject obj =
-	 * createWatsonJSON(status.getText()); } } catch (TwitterException e) {
-	 * e.printStackTrace(); }
-	 * 
-	 * }
-	 **/
-
-	private static String createRequestURLForUser(String username) {
+	private static String createRequestURLForUsername(String username) {
 		String urlString = "https://api.twitter.com/1.1/search/tweets.json?q=" + username
 				+ "&result_type=popular&tweet_mode=extended";
 		return urlString;
 	}
 
-	private static JSONObject createWatsonJSON(String textToAnalyze) {
-		String jsonString = "{\"textToAnalyze\": \"" + textToAnalyze + "\","
-				+ " \"username\"     : \"d74d1640-78d7-4c0b-ba88-ed16e9935e2d\","
-				+ " \"password\"     : \"05icUbUbAXrJ\","
-				+ " \"endpoint\"     : \"https://sandbox-watson-proxy.mybluemix.net/tone-analyzer/api\","
-				+ " \"skip_authentication\": \"true\"}";
-		try {
-			JSONObject obj = new JSONObject(jsonString);
-			return obj;
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		return null;
-
-	}
 }
